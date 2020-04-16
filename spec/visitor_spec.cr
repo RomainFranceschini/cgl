7566a8146209af721e34c14988ebef807f56c84f
Romain Franceschini <franceschini.romain@gmail.com>
2020-04-16 13:08:58 +0200

    add visitor interface

---
+++ spec/visitor_spec.cr
@@ -0,0 +1,64 @@
+require "./spec_helper"
+
+include CGL
+
+private class MyVisitor(V)
+  include Visitor(V)
+
+  getter vertices = Set(V).new
+  getter edges = Set(AnyEdge(V)).new
+
+  def initialize(@visit_vertices = true, @visit_edges = true)
+  end
+
+  def visit_vertices?
+    @visit_vertices
+  end
+
+  def visit_edges?
+    @visit_edges
+  end
+
+  def visit(v : V)
+    @vertices << v
+  end
+
+  def visit(edge : AnyEdge(V))
+    @edges << edge
+  end
+end
+
+describe CGL do
+  describe "while visiting IGraphs" do
+    it "vertices can be visited" do
+      g = DiGraph(Int32).new(edges: { {1, 2}, {1, 6}, {2, 3}, {2, 4}, {4, 5}, {6, 4} })
+      visitor = MyVisitor(Int32).new(true, false)
+      g.accept(visitor)
+      visitor.vertices.should eq(Set{1, 2, 3, 4, 5, 6})
+    end
+
+    it "vertices can be ignored" do
+      g = DiGraph(Int32).new(edges: { {1, 2}, {1, 6}, {2, 3}, {2, 4}, {4, 5}, {6, 4} })
+      visitor = MyVisitor(Int32).new(false, false)
+      g.accept(visitor)
+      visitor.vertices.should be_empty
+    end
+
+    it "edges can be visited" do
+      g = DiGraph(Int32).new(edges: { {1, 2}, {1, 6}, {2, 3}, {2, 4}, {4, 5}, {6, 4} })
+      visitor = MyVisitor(Int32).new(false, true)
+      g.accept(visitor)
+      (visitor.edges == Set{
+        DiEdge.new(1, 2), DiEdge.new(1, 6), DiEdge.new(2, 3),
+        DiEdge.new(2, 4), DiEdge.new(4, 5), DiEdge.new(6, 4),
+      }).should be_true
+    end
+
+    it "vertices can be ignored" do
+      g = DiGraph(Int32).new(edges: { {1, 2}, {1, 6}, {2, 3}, {2, 4}, {4, 5}, {6, 4} })
+      visitor = MyVisitor(Int32).new(false, false)
+      g.accept(visitor)
+      visitor.edges.should be_empty
+    end
+  end
+end
---
+++ src/cgl/traversal/visitor.cr
@@ -0,0 +1,31 @@
+module CGL
+  # A 'Visitor' provides
+  #
+  # Note: if you care about ordering, use an iterator instead.
+  module Visitor(V)
+    abstract def visit(visitable)
+
+    def visit_vertices? : Bool
+      false
+    end
+
+    def visit_edges? : Bool
+      true
+    end
+
+    def accept(graph : IGraph(V))
+      visitable.accept(self)
+    end
+  end
+
+  module IGraph(V)
+    def accept(visitor : Visitor(V))
+      if visitor.visit_vertices?
+        each_vertex { |v| visitor.visit(v) }
+      end
+      if visitor.visit_edges?
+        each_edge { |edge| visitor.visit(edge) }
+      end
+    end
+  end
+end
