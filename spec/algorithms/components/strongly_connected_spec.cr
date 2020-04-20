94955f84e78efcd7235d138e982072179064251a
Romain Franceschini <franceschini.romain@gmail.com>
2020-04-20 12:20:40 +0200

    implement remaining scc algorithms
    * scc iterator
    * scc count
    * scc property

--- spec/algorithms/components/strongly_connected_spec.cr
+++ spec/algorithms/components/strongly_connected_spec.cr
@@ -4,7 +4,37 @@ include CGL
 
 describe CGL do
   describe "DiGraph" do
+    describe "properties" do
+      it "#connected?" do
+        DiGraph(Char).new(edges: {
+          {'a', 'b'}, {'b', 'e'}, {'e', 'a'},             # 1
+          {'b', 'c'}, {'e', 'f'}, {'h', 'g'},             #
+          {'f', 'g'}, {'g', 'f'},                         # 2
+          {'c', 'd'}, {'d', 'c'}, {'d', 'h'}, {'h', 'd'}, # 3
+        }).strongly_connected?.should be_false
+
+        DiGraph(Int32).new(edges: {
+          {1, 2}, {2, 3}, {3, 1},
+        }).strongly_connected?.should be_true
+      end
+    end
+
     describe "strongly connected components" do
+      it "can be counted" do
+        DiGraph(Char).new(edges: {
+          {'a', 'b'}, {'b', 'e'}, {'e', 'a'},             # 1
+          {'b', 'c'}, {'e', 'f'}, {'h', 'g'},             #
+          {'f', 'g'}, {'g', 'f'},                         # 2
+          {'c', 'd'}, {'d', 'c'}, {'d', 'h'}, {'h', 'd'}, # 3
+        }).count_strongly_connected_components.should eq(3)
+
+        DiGraph(Int32).new(edges: {
+          {1, 2}, {2, 3}, {3, 1},
+        }).count_strongly_connected_components.should eq 1
+
+        DiGraph(Int32).new.count_strongly_connected_components.should eq 0
+      end
+
       it "can be enumerated" do
         g = DiGraph(Char).new(edges: {
           {'a', 'b'}, {'b', 'e'}, {'e', 'a'},             # 1
@@ -24,6 +54,21 @@ describe CGL do
           scc.should eq(expected[i])
           i += 1
         end
+      end
+
+      it "can be iterated" do
+        g = DiGraph(Char).new(edges: {
+          {'a', 'b'}, {'b', 'e'}, {'e', 'a'},             # 1
+          {'b', 'c'}, {'e', 'f'}, {'h', 'g'},             #
+          {'f', 'g'}, {'g', 'f'},                         # 2
+          {'c', 'd'}, {'d', 'c'}, {'d', 'h'}, {'h', 'd'}, # 3
+        })
+
+        iterator = g.each_strongly_connected_component
+        iterator.next.should eq ['f', 'g']
+        iterator.next.should eq ['c', 'd', 'h']
+        iterator.next.should eq ['a', 'b', 'e']
+        iterator.next.should be_a(Iterator::Stop)
       end
     end
   end
--- src/cgl/algorithms/components/strongly_connected.cr
+++ src/cgl/algorithms/components/strongly_connected.cr
@@ -59,5 +59,122 @@ module CGL
         end
       end
     end
+
+    # Returns an iterator of **strongly** connected components.
+    def each_strongly_connected_component : Iterator(Array(V))
+      SCCIterator(V).new(self)
+    end
+
+    # Returns the number of **strongly** connected components in `self`.
+    def count_strongly_connected_components : Int32
+      each_strongly_connected_component.size
+    end
+
+    # Whether self is **strongly** connected.
+    def strongly_connected? : Bool
+      return false if empty?
+      scc = each_strongly_connected_component.first
+      scc.size == self.order
+    end
+
+    # :nodoc:
+    private class SCCIterator(V) < GraphIterator(V)
+      include Iterator(Array(V))
+
+      @current_queue : Deque(V)? = nil
+
+      def initialize(@graph : AbstractDiGraph(V))
+        super(@graph)
+
+        @found = Set(V).new
+        @preorder = Hash(V, Int32).new
+        @lowlink = Hash(V, Int32).new
+        @to_visit = Deque(V).new
+        @i = 0
+      end
+
+      private def get_queue : Deque(V)?
+        if queue = @current_queue
+          if queue.empty?
+            if source = next_vertex
+              queue.push(source)
+            else
+              return nil
+            end
+          end
+          queue
+        else
+          if source = next_vertex
+            queue = Deque(V){source}
+            @current_queue = queue
+            return queue
+          end
+          nil
+        end
+      end
+
+      private def next_scc_root
+        if queue = self.get_queue
+          while !queue.empty?
+            u = queue.last
+
+            unless @preorder.has_key?(u)
+              @i += 1
+              @preorder[u] = @i
+            end
+
+            done = true
+            @graph.each_adjacent(u) do |v|
+              unless @preorder.has_key?(v)
+                queue.push(v)
+                done = false
+                break
+              end
+            end
+
+            if done
+              @lowlink[u] = @preorder[u]
+              @graph.each_adjacent(u) do |v|
+                unless @found.includes?(v)
+                  @lowlink[u] = if @preorder[v] > @preorder[u]
+                                  Math.min(@lowlink[u], @lowlink[v])
+                                else
+                                  Math.min(@lowlink[u], @preorder[v])
+                                end
+                end
+              end
+
+              queue.pop # u
+
+              if @lowlink[u] == @preorder[u]
+                return u
+              else
+                @to_visit.push(u)
+              end
+            end
+          end
+        end
+
+        nil
+      end
+
+      def next
+        if u = next_scc_root
+          scc = Array(V){u}
+          while !@to_visit.empty? && @preorder[@to_visit.last] > @preorder[u]
+            v = @to_visit.pop
+            scc << v
+          end
+          @found.concat(scc)
+          scc
+        else
+          stop
+        end
+      end
+
+      def skip_vertex?(v : V) : Bool
+        @found.includes?(v)
+      end
+    end
   end
 end
